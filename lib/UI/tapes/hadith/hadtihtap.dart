b663c94decbb93ed452143b43a35e670950f13b8
abdelrhamanadel4-Flutter <abdelrhamanadel4@gmail.com>
2025-09-08 03:33:18 +0300

    + Initalize project + complete ui screan + complete logic sebha and quran + on borading screan + splash screan

--- lib/UI/tapes/hadith/hadtihtap.dart
+++ lib/UI/tapes/hadith/hadtihtap.dart
@@ -1,20 +1,36 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:projectt/ultilss/app%20assets.dart';
-import 'package:projectt/ultilss/appcolors.dart';
-import 'package:projectt/ultilss/appstyles.dart';
+import 'package:flutter/services.dart';
+import 'package:projectt/ultilss/app%20routes.dart';
+import '../../../ultilss/app assets.dart';
+import '../../../ultilss/appcolors.dart';
+import '../../../ultilss/appstyles.dart';
 
-class Hadtih extends StatelessWidget {
+class Hadtih extends StatefulWidget {
   const Hadtih({super.key});
 
   @override
+  State<Hadtih> createState() => _HadtihState();
+}
+List<String> hadithTitles = [];
+List<String> hadithBodies = [];
+class _HadtihState extends State<Hadtih> {
+  @override
   Widget build(BuildContext context) {
+
     var height=MediaQuery.of(context).size.height;
     var width=MediaQuery.of(context).size.width;
+    if(hadithBodies.isEmpty)
+      loadSuraFile();
     return Expanded(
-      child: ListView.separated(
-        itemBuilder:(context, index) =>  Padding(
-          padding:  EdgeInsets.symmetric(horizontal:width*0.02,vertical: height*0.02 ),
-          child: Container(
+      child: CarouselSlider.builder(itemCount: 50, itemBuilder: (context, index, realIndex) =>
+          InkWell(
+            onTap: () {
+              Navigator.pushNamed(context, AppRoutes.HadithDetails,arguments: index);
+            },
+            child: Padding(
+                    padding:  EdgeInsets.symmetric(horizontal:width*0.02,vertical: height*0.02 ),
+                    child: Container(
             width: width*0.75,
             decoration: BoxDecoration(
                 color: Appcolors.primarycolor,
@@ -24,18 +40,50 @@ class Hadtih extends StatelessWidget {
             child: Column(
               children: [
                 SizedBox(height: height*0.04,),
-                Text('الحد يث الأول',style: AppStyles.bold24black,),
+                Text(hadithTitles[index],style: AppStyles.bold24black,),
                 SizedBox(height: height*0.04,),
-                Text(' عن أمـيـر المؤمنـين أبي حـفص عمر بن الخطاب رضي الله عنه ، قال : سمعت رسول الله صلى الله عـليه وسلم يـقـول : ( إنـما الأعـمـال بالنيات وإنـمـا لكـل امـرئ ما نـوى . فمن كـانت هجرته إلى الله ورسولـه فهجرتـه إلى الله ورسـوله ومن كانت هجرته لـدنيا يصـيبها أو امرأة ينكحها فهجرته إلى ما هاجر إليه ).رواه إمام المحد ثين أبـو عـبـد الله محمد بن إسماعـيل بن ابراهـيـم بن المغـيره بن بـرد زبه البخاري الجعـفي،[رقم:1] وابـو الحسـيـن مسلم بن الحجاج بن مـسلم القـشـيري الـنيسـابـوري [رقم :1907] رضي الله عنهما في صحيحيهما اللذين هما أصح الكتب المصنفه.',style: AppStyles.bold16black,textDirection: TextDirection.rtl,textAlign: TextAlign.center,)
-
+                Expanded(
+                  child: Text(hadithBodies[index],maxLines: 13,
+                    style: AppStyles.bold16black,textDirection: TextDirection.rtl,textAlign: TextAlign.center,),
+                )
 
               ],
             ),
 
-          ),
-        ) ,itemCount: 10,separatorBuilder: (context, index) => SizedBox(),scrollDirection: Axis.horizontal,
-
-      ),
+                    ),
+                  ),
+          ) , options: CarouselOptions(
+        height: 900,
+        aspectRatio: 16/9,
+        viewportFraction: 0.8,
+        initialPage: 0,
+        autoPlayInterval: Duration(seconds: 3),
+        autoPlayAnimationDuration: Duration(milliseconds: 800),
+        autoPlayCurve: Curves.fastOutSlowIn,
+        enlargeCenterPage: true,
+        enlargeFactor: 0.2,
+        scrollDirection: Axis.horizontal,
+      ))
     );
   }
+  Future<void>loadSuraFile()async{
+    List<String> titles = [];
+    List<String> bodies = [];
+    for(int i=0;i<50;i++) {
+      String fileContent = await rootBundle.loadString(
+          'assets/files/Hadeeth/h${i + 1}.txt');
+      List<String>hadtihContent = fileContent.split('\n');
+      String title = hadtihContent.isNotEmpty ? hadtihContent.first.trim() : '';
+      String body = hadtihContent.length > 1 ? hadtihContent.sublist(1).join(
+          '\n').trim() : '';
+    titles.add(title);
+    bodies.add(body);
+      setState(() {
+        hadithTitles=titles;
+        hadithBodies=bodies;
+      });
+    }
+  }
 }
+
+
